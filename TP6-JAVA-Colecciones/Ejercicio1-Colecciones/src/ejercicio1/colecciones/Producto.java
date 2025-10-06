8b5ed9be2e95f9c4394b97f07f9a0969dec2df17
Lautaro Buchek <129329309+lBuKi19@users.noreply.github.com>
2025-10-06 12:04:51 -0300

    Add files via upload

---
+++ TP6-JAVA-Colecciones/Ejercicio1-Colecciones/src/ejercicio1/colecciones/Producto.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio1.colecciones;
+
+/**
+ *
+ * @author lauta
+ */
+public class Producto {
+    String id;
+    String nombre;
+    double precio;
+    int cantidad;
+    CategoriaProducto categoria;
+
+    public Producto(String id, String nombre, double precio, int cantidad, CategoriaProducto categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.categoria = categoria;
+    }
+    
+    public void mostrarInfo() {
+        System.out.println("Producto: "+nombre +", precio: "+precio +", categoria: "+categoria);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public CategoriaProducto getCategoria() {
+        return categoria;
+    }
+    
+   
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", cantidad=" + cantidad + ", categoria=" + categoria + '}';
+    }
+    
+    
+    
+    
+}
